7a7dc3522336ba6f2f9f0d7d5df6c6b6a3f38385
DarkMiata <samvermeulen@gmail.com>
2017-06-30 18:04:57 +0200

    consolidation du code: mise à jour de départ

--- src/OC/PlatformBundle/Entity/Advert.php
+++ src/OC/PlatformBundle/Entity/Advert.php
@@ -2,20 +2,18 @@
 
 namespace OC\PlatformBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+// N'oubliez pas ce use :
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
- * @ORM\Table(name="advert")
+ * @ORM\Table(name="oc_advert")
  * @ORM\Entity(repositoryClass="OC\PlatformBundle\Repository\AdvertRepository")
+ * @ORM\HasLifecycleCallbacks()
  */
 class Advert
 {
-  /**
-   * @ORM\OneToOne(targetEntity="OC\PlatformBundle\Entity\Image", cascade={"persist"})
-   * @ORM\JoinColumn(nullable=false)
-   */
-  private $image;
-
   /**
    * @var int
    *
@@ -56,12 +54,63 @@ class Advert
   /**
    * @ORM\Column(name="published", type="boolean")
    */
-  private $published;
+  private $published = true;
+
+  /**
+   * @ORM\OneToOne(targetEntity="OC\PlatformBundle\Entity\Image", cascade={"persist"})
+   */
+  private $image;
+
+  /**
+   * @ORM\ManyToMany(targetEntity="OC\PlatformBundle\Entity\Category", cascade={"persist"})
+   * @ORM\JoinTable(name="oc_advert_category")
+   */
+  private $categories;
+
+  /**
+   * @ORM\OneToMany(targetEntity="OC\PlatformBundle\Entity\Application", mappedBy="advert")
+   */
+  private $applications; // Notez le « s », une annonce est liée à plusieurs candidatures
+
+  /**
+   * @ORM\Column(name="updated_at", type="datetime", nullable=true)
+   */
+  private $updatedAt;
+
+  /**
+   * @ORM\Column(name="nb_applications", type="integer")
+   */
+  private $nbApplications = 0;
+
+  /**
+   * @Gedmo\Slug(fields={"title"})
+   * @ORM\Column(name="slug", type="string", length=255, unique=true)
+   */
+  private $slug;
 
   public function __construct()
   {
-    // Par défaut, la date de l'annonce est la date d'aujourd'hui
-    $this->date = new \Datetime();
+    $this->date         = new \Datetime();
+    $this->categories   = new ArrayCollection();
+    $this->applications = new ArrayCollection();
+  }
+
+  /**
+   * @ORM\PreUpdate
+   */
+  public function updateDate()
+  {
+    $this->setUpdatedAt(new \Datetime());
+  }
+
+  public function increaseApplication()
+  {
+    $this->nbApplications++;
+  }
+
+  public function decreaseApplication()
+  {
+    $this->nbApplications--;
   }
 
   /**
@@ -137,50 +186,127 @@ class Advert
   }
 
   /**
-   * Set published
-   *
-   * @param boolean $published
-   *
-   * @return Advert
+   * @param bool $published
    */
   public function setPublished($published)
   {
-      $this->published = $published;
-
-      return $this;
+    $this->published = $published;
   }
 
   /**
-   * Get published
-   *
-   * @return boolean
+   * @return bool
    */
   public function getPublished()
   {
-      return $this->published;
+    return $this->published;
   }
 
-  /**
-   * Set image
-   *
-   * @param \OC\PlatformBundle\Entity\Image $image
-   *
-   * @return Advert
-   */
-  public function setImage(\OC\PlatformBundle\Entity\Image $image)
+  public function setImage(Image $image = null)
   {
-      $this->image = $image;
-
-      return $this;
+    $this->image = $image;
   }
 
-  /**
-   * Get image
-   *
-   * @return \OC\PlatformBundle\Entity\Image
-   */
   public function getImage()
   {
-      return $this->image;
+    return $this->image;
+  }
+
+  /**
+   * @param Category $category
+   */
+  public function addCategory(Category $category)
+  {
+    $this->categories[] = $category;
+  }
+
+  /**
+   * @param Category $category
+   */
+  public function removeCategory(Category $category)
+  {
+    $this->categories->removeElement($category);
+  }
+
+  /**
+   * @return ArrayCollection
+   */
+  public function getCategories()
+  {
+    return $this->categories;
+  }
+
+  /**
+   * @param Application $application
+   */
+  public function addApplication(Application $application)
+  {
+    $this->applications[] = $application;
+
+    // On lie l'annonce à la candidature
+    $application->setAdvert($this);
+  }
+
+  /**
+   * @param Application $application
+   */
+  public function removeApplication(Application $application)
+  {
+    $this->applications->removeElement($application);
+  }
+
+  /**
+   * @return \Doctrine\Common\Collections\Collection
+   */
+  public function getApplications()
+  {
+    return $this->applications;
+  }
+
+  /**
+   * @param \DateTime $updatedAt
+   */
+  public function setUpdatedAt(\Datetime $updatedAt = null)
+  {
+      $this->updatedAt = $updatedAt;
+  }
+
+  /**
+   * @return \DateTime
+   */
+  public function getUpdatedAt()
+  {
+      return $this->updatedAt;
+  }
+
+  /**
+   * @param integer $nbApplications
+   */
+  public function setNbApplications($nbApplications)
+  {
+      $this->nbApplications = $nbApplications;
+  }
+
+  /**
+   * @return integer
+   */
+  public function getNbApplications()
+  {
+      return $this->nbApplications;
+  }
+
+  /**
+   * @param string $slug
+   */
+  public function setSlug($slug)
+  {
+      $this->slug = $slug;
+  }
+
+  /**
+   * @return string
+   */
+  public function getSlug()
+  {
+      return $this->slug;
   }
 }
